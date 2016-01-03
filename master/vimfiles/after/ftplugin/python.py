d8b6fd2c9a56f456fad392b39f87c4f201948d29
Akinori Hattori <hattya@gmail.com>
2016-01-03 14:48:26 +0900

    master: add new file vimfiles/after/ftplugin/python.vim

---
+++ master/vimfiles/after/ftplugin/python.py
@@ -0,0 +1,54 @@
+# after/ftplugin/python.py
+
+import io
+import os
+import re
+import sys
+
+import vim
+
+
+_vimrc_pth_re = re.compile(r'[\s,]')
+
+
+def _vimrc_pth(activate):
+    def find(path, cands):
+        while True:
+            for n in cands:
+                pth = os.path.join(path, n)
+                if os.path.isfile(pth):
+                    return pth
+            path, p = os.path.dirname(path), path
+            if path == p:
+                return
+
+    def repl(m):
+        s = m.group(0)
+        return '\\' * (3 if s.isspace() else 2) + s
+
+    cands = ['.pth']
+    if sys.platform == 'win32':
+        cands.insert(0, '_pth')
+    pth = find(os.path.dirname(vim.current.buffer.name) or os.getcwd(), cands)
+    if pth:
+        root = os.path.dirname(pth)
+        values = {'pyver': '{}.{}'.format(*sys.version_info)}
+        i = 1 if 'jedi.api' in sys.modules else 0
+        with io.open(pth, encoding='utf-8-sig') as fp:
+            for l in fp:
+                l = l.strip().format(**values)
+                p = os.path.normpath(os.path.join(root, l))
+                if not os.path.isdir(p):
+                    continue
+                elif activate:
+                    if p not in sys.path:
+                        sys.path.insert(i, p)
+                        i += 1
+                else:
+                    if p in sys.path:
+                        sys.path.remove(p)
+
+    path = [_vimrc_pth_re.sub(repl, p) for p in sys.path if os.path.isdir(p)]
+    vim.command('setlocal path=.,{},,'.format(','.join(path)))
+    if 'setl path<' not in vim.eval('b:undo_ftplugin'):
+        vim.command('let b:undo_ftplugin .= " | setl path<"')
---
+++ master/vimfiles/after/ftplugin/python.vim
@@ -0,0 +1,45 @@
+" after/ftplugin/python.vim
+
+if exists('b:undo_ftplugin')
+  let b:undo_ftplugin .= ' | '
+else
+  let b:undo_ftplugin = ''
+endif
+
+setlocal expandtab
+setlocal fileformat=unix
+setlocal formatoptions-=t
+setlocal shiftwidth=4
+setlocal tabstop=4
+setlocal textwidth=79
+
+let b:undo_ftplugin .= 'setl et< ff< fo< sw< ts< tw<'
+
+let g:python_highlight_all = 1
+
+let s:script = expand('<sfile>:p:r') . '.py'
+if has('python3')
+  execute 'py3file ' . s:script
+elseif has('python')
+  execute 'pyfile ' . s:script
+else
+  finish
+endif
+
+function! s:sid() abort
+  return matchstr(expand('<sfile>'), '<SNR>\zs\d\+\ze_SID$')
+endfunction
+let s:sid = s:sid()
+
+augroup vimrc-ft-python
+  autocmd! * <buffer>
+  autocmd BufEnter <buffer> call s:pth(1)
+  autocmd BufLeave <buffer> call s:pth(0)
+augroup END
+
+function! s:pth(activate)
+  execute printf('%s _vimrc_pth(int(vim.eval("a:activate")))', has('python3') ? 'python3' : 'python')
+endfunction
+
+let b:undo_ftplugin .= ' | execute "autocmd! vimrc-ft-python * <buffer>"'
+let b:undo_ftplugin .= ' | call <SNR>' . s:sid . '_pth(0)'
