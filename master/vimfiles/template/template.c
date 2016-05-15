f9ff506801284b6edc1ea5ab8ec6d10a2daaabed
Akinori Hattori <hattya@gmail.com>
2016-05-15 18:28:58 +0900

    master: add new file vimfiles/template/template.c

---
+++ master/vimfiles/template/template.c
@@ -0,0 +1,14 @@
+/*
+ * <%= expand('%:gs?\\?/?') %>
+ *
+ *   Copyright (c) <%= strftime('%Y') %> <%= g:user.format() %>
+ */
+
+#include <stdio.h>
+
+
+int
+main(int argc, char *argv[]) {
+    <+CURSOR+>
+    return 0;
+}
