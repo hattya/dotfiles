aa4775c32fe55c506bf0cf023fac327501797111
Akinori Hattori <hattya@gmail.com>
2016-05-15 18:29:37 +0900

    master: add new file vimfiles/template/template.h

---
+++ master/vimfiles/template/template.h
@@ -0,0 +1,25 @@
+:silent %s/<+INCLUDE_GUARD+>/\=toupper(expand('%:r:gs?\\?\/?:gs?\/?_?')) . '_H'/ge
+/*
+ * <%= expand('%:gs?\\?/?') %>
+ *
+ *   Copyright (c) <%= strftime('%Y') %> <%= g:user.format() %>
+ */
+
+#ifndef <+INCLUDE_GUARD+>
+# define <+INCLUDE_GUARD+>
+
+
+
+# ifdef __cplusplus
+extern "C" {
+# endif /* __cplusplus */
+
+
+<+CURSOR+>
+
+
+# ifdef __cplusplus
+}
+# endif /* __cplusplus */
+
+#endif /* <+INCLUDE_GUARD+> */
