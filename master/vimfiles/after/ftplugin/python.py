0fcbce5483dcac4a89aa55548567648bd5bf2ebd
Akinori Hattori <hattya@gmail.com>
2018-10-02 22:41:34 +0900

    master: Vim: improve filetype plugin for python

--- master/vimfiles/after/ftplugin/python.py
+++ master/vimfiles/after/ftplugin/python.py
@@ -30,25 +30,23 @@ def _vimrc_pth(activate):
     if sys.platform == 'win32':
         cands.insert(0, '_pth')
     pth = find(os.path.dirname(vim.current.buffer.name) or os.getcwd(), cands)
+    pth_path = []
     if pth:
         root = os.path.dirname(pth)
         values = {'pyver': '{}.{}'.format(*sys.version_info)}
-        i = 1 if 'jedi.api' in sys.modules else 0
         with io.open(pth, encoding='utf-8-sig') as fp:
             for l in fp:
                 l = os.path.expandvars(l.strip().format(**values))
                 p = os.path.normpath(os.path.join(root, l))
-                if not os.path.isdir(p):
-                    continue
-                elif activate:
-                    if p not in sys.path:
-                        sys.path.insert(i, p)
-                        i += 1
-                else:
-                    if p in sys.path:
-                        sys.path.remove(p)
+                if (os.path.isdir(p)
+                    and p not in sys.path):
+                    pth_path.append(p)
+        # for Jedi
+        m = sys.modules.get('jedi.api.project')
+        if m is not None:
+            m.discover_buildout_paths = lambda *a, **kw: pth_path
 
-    path = [_vimrc_pth_re.sub(repl, p) for p in sys.path if os.path.isdir(p)]
+    path = [_vimrc_pth_re.sub(repl, p) for p in sys.path + pth_path if os.path.isdir(p)]
     vim.command('setlocal path=.,{},,'.format(','.join(path)))
     if 'setl path<' not in vim.eval('b:undo_ftplugin'):
         vim.command('let b:undo_ftplugin .= " | setl path<"')
