dd4408075c44d6772e7c99fcd7d7846e24573526
Akinori Hattori <hattya@gmail.com>
2021-08-15 20:51:23 +0900

    master: add new file vimfiles/template/template.pyi

---
+++ master/vimfiles/template/template.pyi
@@ -0,0 +1,15 @@
+#
+# <%= expand('%:r:gs?[/\\]?.?') %>
+#
+#   Copyright (c) <%= strftime('%Y') %> <%= g:user.format() %>
+#
+
+from typing import cast, Any, Optional, Union
+
+
+def <+CURSOR+>() -> None:
+    ...
+
+
+class _:
+    ...
